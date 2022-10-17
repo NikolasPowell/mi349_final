9d245ceb461e6c57fb21e496e92b46d0b669d51d
Nikolas Powell <npowell922@comcast.net>
2022-10-17 10:41:53 -0400

    changes

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
+<link rel="javascript" href="gallery.html">
+
 function enlargeImg() {
     var newerImg = document.getElementById("furniture")
     newerImg = newerImg.width * 2;
-    newerImg = newImg.heigt * 2;
+    newerImg = newerImg.heigt * 2;
 }
