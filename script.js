c646d4dbd38c47432dc1c6d53d17e67f8c92024e
Nikolas Powell <npowell922@comcast.net>
2022-10-19 11:19:24 -0400

    changes

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function enlargeImg() {
+/*function enlargeImg() {
     var newerImg = document.getElementById("furniture")
     newerImg = newerImg.width * 2;
     newerImg = newerImg.heigt * 2;
@@ -8,4 +8,20 @@ function enlargeImg2() {
     var newerImg = document.getElementById("furniture2")
     newerImg = newerImg.width * 2;
     newerImg = newerImg.heigt * 2;
+}*/
+
+var image = document.getElementById('image');
+
+var imageClose = document.getElementById('image-close');
+imageClose.addEventListener('click', function()) {
+    image.style.display = "none";
 }
+
+document.addEventListener('click', function (e) {
+    if (e.target.className.indexOf('image-target') !== -1){
+        var img = e.target;
+        var imageContent = document.getElementById("image-content");
+        image.style.display = "block";
+        imageContent.src = img.src;
+    }
+});
