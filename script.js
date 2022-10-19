df29e037d28999ac45334cf03a8626a2dd910009
Nikolas Powell <npowell922@comcast.net>
2022-10-19 11:22:50 -0400

    change

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ imageClose.addEventListener('click', function()) {
 }
 
 document.addEventListener('click', function (e) {
-    if (e.target.className.indexOf('image-target') !== -1){
+    if (e.target.className.indexOf('furniture') !== -1){
         var img = e.target;
         var imageContent = document.getElementById("image-content");
         image.style.display = "block";
